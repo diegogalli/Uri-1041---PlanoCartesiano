0f33b4409a5437ae0bd84ce225ff7f033fd5abf6
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:55:23 -0300

    Uri 1041 Plano Cartesiano

---
+++ CoordenadasPonto/CoordenadasPonto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoordenadasPonto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ponto = Console.ReadLine().Split(' ');
+
+            double x = double.Parse(ponto[0]);
+            double y = double.Parse(ponto[1]);
+
+            if ( x == 0.0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0.0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if ( y == 0.0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if(x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
